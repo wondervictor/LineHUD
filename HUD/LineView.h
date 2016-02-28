04b82ada5dea79a333584d292edfdc36cb194d5e
VicChan <victorchanchina@gmail.com>
2016-02-28 14:35:50 +0800

    Line HUD

---
+++ HUD/LineView.h
@@ -0,0 +1,13 @@
+//
+//  LineView.h
+//  LineHUD
+//
+//  Created by VicChan on 16/2/27.
+//  Copyright © 2016年 VicChan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LineView : UIView
+
+@end
---
+++ HUD/LineView.m
@@ -0,0 +1,21 @@
+//
+//  LineView.m
+//  LineHUD
+//
+//  Created by VicChan on 16/2/27.
+//  Copyright © 2016年 VicChan. All rights reserved.
+//
+
+#import "LineView.h"
+
+@implementation LineView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
